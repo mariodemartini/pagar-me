d5c0a61672c26eaba28171a23435e600ee43295b
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-07 08:18:45 -0300

    adicionando metodo listarTransacoes na controller

--- src/main/java/br/com/geradordedevs/pagarme/controllers/TransacoesController.java
+++ src/main/java/br/com/geradordedevs/pagarme/controllers/TransacoesController.java
@@ -1,10 +1,23 @@
 package br.com.geradordedevs.pagarme.controllers;
 
+import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
+import br.com.geradordedevs.pagarme.services.TransacoesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transacoes")
 public class TransacoesController {
+    @Autowired
+    private TransacoesService transacoesService;
+
+    @GetMapping
+    public List<TransacoesEntity> listarTransacoes(){
+        return transacoesService.listaTransacoes();
+    }
 
 }
