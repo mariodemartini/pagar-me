0c7f25c01fb3cb39540d8e3eb93c70c3e6c7bcba
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-07 08:09:19 -0300

    criando a camada TransacoesService

---
+++ src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
@@ -0,0 +1,4 @@
+package br.com.geradordedevs.pagarme.services;
+
+public interface TransacoesService {
+}
---
+++ src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
@@ -0,0 +1,10 @@
+package br.com.geradordedevs.pagarme.services.impl;
+
+import br.com.geradordedevs.pagarme.services.TransacoesService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class TransacoesServiceImpl implements TransacoesService {
+}
