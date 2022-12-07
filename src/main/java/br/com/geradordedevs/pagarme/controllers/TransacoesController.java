69f1b1cb03330a73098b928b248a56da7a7e8472
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-07 08:16:28 -0300

    criando a camada TransacoesController

---
+++ src/main/java/br/com/geradordedevs/pagarme/controllers/TransacoesController.java
@@ -0,0 +1,10 @@
+package br.com.geradordedevs.pagarme.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacoes")
+public class TransacoesController {
+
+}
