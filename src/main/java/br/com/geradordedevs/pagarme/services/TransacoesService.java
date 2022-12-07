faa77b002b6091085ca75e8de953353c76639ef6
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-07 08:16:54 -0300

    criando o metodo listarTransacoes na service

--- src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
@@ -1,4 +1,9 @@
 package br.com.geradordedevs.pagarme.services;
 
+import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
+
+import java.util.List;
+
 public interface TransacoesService {
+    List<TransacoesEntity> listaTransacoes();
 }
--- src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
@@ -1,10 +1,28 @@
 package br.com.geradordedevs.pagarme.services.impl;
 
+import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
+import br.com.geradordedevs.pagarme.repositories.TransacoesRepository;
 import br.com.geradordedevs.pagarme.services.TransacoesService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
 public class TransacoesServiceImpl implements TransacoesService {
+
+    @Autowired
+    private TransacoesRepository transacoesRepository;
+    @Override
+    public List<TransacoesEntity> listaTransacoes() {
+        log.info("listando as transações");
+        List<TransacoesEntity> transacoesEntities = new ArrayList<>();
+        for (TransacoesEntity transacoesEntity : transacoesRepository.findAll()) {
+            transacoesEntities.add(transacoesEntity);
+        }
+        return transacoesEntities;
+    }
 }
