65601fc897e6ecefb6c5e6fff6e67bda4d88ba8d
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-08 15:38:46 -0300

    atualizando metodo cadastrarTransacao

--- src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
@@ -4,7 +4,9 @@ import br.com.geradordedevs.pagarme.dtos.requests.TransacoesRequestDTO;
 import br.com.geradordedevs.pagarme.dtos.responses.TransacoesResponseDTO;
 import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
 import br.com.geradordedevs.pagarme.mapper.TransacoesMapper;
+import br.com.geradordedevs.pagarme.repositories.PagamentoRepository;
 import br.com.geradordedevs.pagarme.repositories.TransacoesRepository;
+import br.com.geradordedevs.pagarme.services.PagamentoService;
 import br.com.geradordedevs.pagarme.services.TransacoesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +15,21 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static br.com.geradordedevs.pagarme.enums.MetodoPagamentoEnum.CREDIT_CARD;
+import static br.com.geradordedevs.pagarme.enums.MetodoPagamentoEnum.DEBIT_CARD;
+
 @Service
 @Slf4j
 public class TransacoesServiceImpl implements TransacoesService {
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
 
     @Autowired
     private TransacoesRepository transacoesRepository;
     @Autowired
     private TransacoesMapper transacoesMapper;
+    @Autowired
+    private PagamentoService pagamentoService;
     @Override
     public List<TransacoesResponseDTO> listaTransacoes() {
         log.info("listando as transações");
@@ -33,7 +42,11 @@ public class TransacoesServiceImpl implements TransacoesService {
 
     @Override
     public TransacoesResponseDTO cadastrarTransacao(TransacoesRequestDTO requestDTO){
-        log.info("cadastrando nova transação: {}", requestDTO);
+        if (pagamentoService.criarPagamento(DEBIT_CARD)){
+            requestDTO.setMetodoPagamentoEnum(DEBIT_CARD);
+        } else {
+            requestDTO.setMetodoPagamentoEnum(CREDIT_CARD);
+        }
         return transacoesMapper.paraDTO(transacoesRepository.save(transacoesMapper.paraEntidade(requestDTO)));
     }
 
