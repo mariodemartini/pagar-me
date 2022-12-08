c257c86ffa4a706e9ca439ffd5f87aaf9ef2248f
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-08 15:39:04 -0300

    atualizando TransacoesService

--- src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
@@ -2,7 +2,6 @@ package br.com.geradordedevs.pagarme.services;
 
 import br.com.geradordedevs.pagarme.dtos.requests.TransacoesRequestDTO;
 import br.com.geradordedevs.pagarme.dtos.responses.TransacoesResponseDTO;
-import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
 
 import java.util.List;
 
