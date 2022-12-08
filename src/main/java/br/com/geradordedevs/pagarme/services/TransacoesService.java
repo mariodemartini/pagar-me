57156d32b8ab0240947a189b8bb507e1f5d0fc25
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-08 14:33:06 -0300

    implementando DTO na service

--- src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
@@ -1,10 +1,12 @@
 package br.com.geradordedevs.pagarme.services;
 
+import br.com.geradordedevs.pagarme.dtos.requests.TransacoesRequestDTO;
+import br.com.geradordedevs.pagarme.dtos.responses.TransacoesResponseDTO;
 import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
 
 import java.util.List;
 
 public interface TransacoesService {
-    List<TransacoesEntity> listaTransacoes();
-    TransacoesEntity cadastrarTransacao(TransacoesEntity transacoesEntity);
+    List<TransacoesResponseDTO> listaTransacoes();
+    TransacoesResponseDTO cadastrarTransacao(TransacoesRequestDTO requestDTO);
 }
--- src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/impl/TransacoesServiceImpl.java
@@ -1,6 +1,9 @@
 package br.com.geradordedevs.pagarme.services.impl;
 
+import br.com.geradordedevs.pagarme.dtos.requests.TransacoesRequestDTO;
+import br.com.geradordedevs.pagarme.dtos.responses.TransacoesResponseDTO;
 import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
+import br.com.geradordedevs.pagarme.mapper.TransacoesMapper;
 import br.com.geradordedevs.pagarme.repositories.TransacoesRepository;
 import br.com.geradordedevs.pagarme.services.TransacoesService;
 import lombok.extern.slf4j.Slf4j;
@@ -16,19 +19,22 @@ public class TransacoesServiceImpl implements TransacoesService {
 
     @Autowired
     private TransacoesRepository transacoesRepository;
+    @Autowired
+    private TransacoesMapper transacoesMapper;
     @Override
-    public List<TransacoesEntity> listaTransacoes() {
+    public List<TransacoesResponseDTO> listaTransacoes() {
         log.info("listando as transações");
         List<TransacoesEntity> transacoesEntities = new ArrayList<>();
         for (TransacoesEntity transacoesEntity : transacoesRepository.findAll()) {
             transacoesEntities.add(transacoesEntity);
         }
-        return transacoesEntities;
+        return transacoesMapper.paraListaDTO(transacoesEntities);
     }
 
     @Override
-    public TransacoesEntity cadastrarTransacao(TransacoesEntity transacoesEntity){
-        log.info("cadastrando nova transação: {}", transacoesEntity);
-        return transacoesRepository.save(transacoesEntity);
+    public TransacoesResponseDTO cadastrarTransacao(TransacoesRequestDTO requestDTO){
+        log.info("cadastrando nova transação: {}", requestDTO);
+        return transacoesMapper.paraDTO(transacoesRepository.save(transacoesMapper.paraEntidade(requestDTO)));
     }
+
 }
