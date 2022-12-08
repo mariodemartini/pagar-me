1ac8922f7abe630b9dfc0eb3e43f08ea275d6866
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-08 15:35:50 -0300

    criando PagamentoResponseDTO

---
+++ src/main/java/br/com/geradordedevs/pagarme/dtos/responses/PagamentoResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.geradordedevs.pagarme.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagamentoResponseDTO {
+    private Long id;
+    private String status;
+    private LocalDate dataPagamento;
+}
