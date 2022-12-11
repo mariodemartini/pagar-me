939fc53c768b5c3b6d50dbeadfa027cfa0a24b5d
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-11 17:07:03 -0300

    testando outro retorno do metodo

--- src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/TransacoesService.java
@@ -2,10 +2,12 @@ package br.com.geradordedevs.pagarme.services;
 
 import br.com.geradordedevs.pagarme.dtos.requests.TransacoesRequestDTO;
 import br.com.geradordedevs.pagarme.dtos.responses.TransacoesResponseDTO;
+import br.com.geradordedevs.pagarme.entities.TransacoesEntity;
 
 import java.util.List;
 
 public interface TransacoesService {
-    List<TransacoesResponseDTO> listaTransacoes();
-    TransacoesResponseDTO cadastrarTransacao(TransacoesRequestDTO requestDTO);
+
+    List<TransacoesEntity> listaTransacoes();
+    TransacoesEntity cadastrarTransacao(TransacoesEntity transacoes);
 }
