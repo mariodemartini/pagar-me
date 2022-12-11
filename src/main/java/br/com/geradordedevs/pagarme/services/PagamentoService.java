51a5002f9c7cc09cf366f8d7258cdfc61410436d
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-12-11 17:06:41 -0300

    atualizando o nome dos parametros

--- src/main/java/br/com/geradordedevs/pagarme/services/PagamentoService.java
+++ src/main/java/br/com/geradordedevs/pagarme/services/PagamentoService.java
@@ -1,9 +1,10 @@
 package br.com.geradordedevs.pagarme.services;
 
-import br.com.geradordedevs.pagarme.dtos.responses.PagamentoResponseDTO;
+import br.com.geradordedevs.pagarme.entities.PagamentoEntity;
 import br.com.geradordedevs.pagarme.enums.MetodoPagamentoEnum;
 
 public interface PagamentoService {
 
-    PagamentoResponseDTO criarPagamento(MetodoPagamentoEnum metodoPagamentoEnum);
+    PagamentoEntity criarPagamento(MetodoPagamentoEnum metodoPagamento);
+
 }
