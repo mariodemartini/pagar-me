0d0b8b169532f7d2d0e104605f7b13de4c1edb20
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-04 15:58:36 -0300

    :recycle:  refactor: adicionando anotacoes da bean validation

--- src/main/java/br/com/geradordedevs/pagarme/dtos/requests/TransacoesRequestDTO.java
+++ src/main/java/br/com/geradordedevs/pagarme/dtos/requests/TransacoesRequestDTO.java
@@ -6,19 +6,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransacoesRequestDTO {
+    @NotNull
     private BigDecimal valorTransacao;
+    @NotBlank
     private String descricaoTransacao;
+    @NotNull
     private MetodoPagamentoEnum metodoPagamento;
+    @NotBlank
+    @Size(min = 16, max = 16, message = "numero do cartao invalido")
     private String numeroCartao;
+    @NotBlank
     private String nomePortador;
+    @NotNull
     private String validadeCartao;
+    @Size(min = 3, max = 3, message = "codigo de segurança invalido")
     private String cvv;
+    @NotNull
     private PagamentoEntity pagamento;
 
     @Override
