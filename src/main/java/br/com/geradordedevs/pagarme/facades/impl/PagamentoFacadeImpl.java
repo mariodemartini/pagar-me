57689fa52bfa0cd663a093a89a194bdfdfa3ae9e
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-04 15:59:12 -0300

    :recycle:  refactor: adicionando tratamento de erro personalizado

--- src/main/java/br/com/geradordedevs/pagarme/facades/impl/PagamentoFacadeImpl.java
+++ src/main/java/br/com/geradordedevs/pagarme/facades/impl/PagamentoFacadeImpl.java
@@ -2,6 +2,8 @@ package br.com.geradordedevs.pagarme.facades.impl;
 
 import br.com.geradordedevs.pagarme.dtos.responses.PagamentoResponseDTO;
 import br.com.geradordedevs.pagarme.enums.MetodoPagamentoEnum;
+import br.com.geradordedevs.pagarme.exceptions.PagamentoException;
+import br.com.geradordedevs.pagarme.exceptions.enums.PagamentoEnum;
 import br.com.geradordedevs.pagarme.facades.PagamentoFacade;
 import br.com.geradordedevs.pagarme.mapper.PagamentoMapper;
 import br.com.geradordedevs.pagarme.services.PagamentoService;
@@ -16,6 +18,9 @@ public class PagamentoFacadeImpl implements PagamentoFacade {
 
     @Override
     public PagamentoResponseDTO criarPagamento(MetodoPagamentoEnum metodoPagamento) {
+        if(metodoPagamento != MetodoPagamentoEnum.CREDIT_CARD || metodoPagamento != MetodoPagamentoEnum.DEBIT_CARD){
+            throw new PagamentoException(PagamentoEnum.PAGAMENTO_INVALIDO);
+        }
         return pagamentoMapper.paraDTO(pagamentoService.criarPagamento(metodoPagamento));
     }
 }
