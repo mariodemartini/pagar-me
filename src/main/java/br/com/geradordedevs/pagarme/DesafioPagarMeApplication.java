e101a8db1c556aa802a77cc4a3c2cf35f1a48348
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-04 15:58:13 -0300

    :recycle:  refactor: adicionando anotacao para bean valitation

--- src/main/java/br/com/geradordedevs/pagarme/DesafioPagarMeApplication.java
+++ src/main/java/br/com/geradordedevs/pagarme/DesafioPagarMeApplication.java
@@ -3,9 +3,10 @@ package br.com.geradordedevs.pagarme;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DesafioPagarMeApplication {
 
 	public static void main(String[] args) {
