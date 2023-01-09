9e5a71d6d570845d85f15474b99aeb9e80360270
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-09 14:11:37 -0300

    :recycle:  refactor: atualizando metodo criarPagamento

--- src/main/java/br/com/geradordedevs/pagarme/facades/impl/PagamentoFacadeImpl.java
+++ src/main/java/br/com/geradordedevs/pagarme/facades/impl/PagamentoFacadeImpl.java
@@ -3,12 +3,15 @@ package br.com.geradordedevs.pagarme.facades.impl;
 import br.com.geradordedevs.pagarme.dtos.responses.PagamentoResponseDTO;
 import br.com.geradordedevs.pagarme.entities.PagamentoEntity;
 import br.com.geradordedevs.pagarme.enums.MetodoPagamentoEnum;
+import br.com.geradordedevs.pagarme.enums.StatusPagamentoEnum;
 import br.com.geradordedevs.pagarme.facades.PagamentoFacade;
 import br.com.geradordedevs.pagarme.mapper.PagamentoMapper;
 import br.com.geradordedevs.pagarme.services.PagamentoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @Slf4j
@@ -23,11 +26,13 @@ public class PagamentoFacadeImpl implements PagamentoFacade {
     public PagamentoResponseDTO criarPagamento(MetodoPagamentoEnum metodoPagamento) {
         PagamentoEntity pagamento = new PagamentoEntity();
         if (metodoPagamento == MetodoPagamentoEnum.CREDIT_CARD){
-            pagamentoService.salvarPagamento(pagamento);
+            pagamento.setDataPagamento(LocalDate.now());
+            pagamento.setStatus(StatusPagamentoEnum.WAITING_FUNDS);
         }
         if (metodoPagamento == MetodoPagamentoEnum.DEBIT_CARD){
-            pagamentoService.salvarPagamento(pagamento);
+            pagamento.setDataPagamento(LocalDate.now().plusDays(30));
+            pagamento.setStatus(StatusPagamentoEnum.PAID);
         }
-        return pagamentoMapper.paraDTO(pagamento);
+        return pagamentoMapper.paraDTO(pagamentoService.salvarPagamento(pagamento));
     }
 }
