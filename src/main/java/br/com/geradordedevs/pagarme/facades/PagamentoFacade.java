18f9d84aa2a0b3530b478aae7ff94926b5a1f907
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-09 14:46:31 -0300

    :recycle:  refactor: removendo camada facade

--- src/main/java/br/com/geradordedevs/pagarme/facades/PagamentoFacade.java
+++
@@ -1,9 +0,0 @@
-package br.com.geradordedevs.pagarme.facades;
-
-import br.com.geradordedevs.pagarme.dtos.responses.PagamentoResponseDTO;
-import br.com.geradordedevs.pagarme.enums.MetodoPagamentoEnum;
-
-public interface PagamentoFacade {
-    PagamentoResponseDTO criarPagamento(MetodoPagamentoEnum metodoPagamento);
-
-}
