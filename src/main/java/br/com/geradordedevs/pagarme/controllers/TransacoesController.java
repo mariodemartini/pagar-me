31a12111aec5fcb6c7993d66e256fb7288ce7f6b
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-11 17:29:58 -0300

    :recycle:  refactor: adicionando anotacoes @ApiResponse

--- src/main/java/br/com/geradordedevs/pagarme/controllers/TransacoesController.java
+++ src/main/java/br/com/geradordedevs/pagarme/controllers/TransacoesController.java
@@ -4,7 +4,11 @@ import br.com.geradordedevs.pagarme.dtos.requests.TransacoesRequestDTO;
 import br.com.geradordedevs.pagarme.dtos.responses.SaldoResponseDTO;
 import br.com.geradordedevs.pagarme.dtos.responses.TransacoesResponseDTO;
 import br.com.geradordedevs.pagarme.facades.TransacoesFacade;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,25 +20,43 @@ public class TransacoesController {
     @Autowired
     private TransacoesFacade transacoesFacade;
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "find all successfuly"),
+            @ApiResponse(code = 400, message = "bad request")
+    })
     @GetMapping
-    public List<TransacoesResponseDTO> listarTransacoes(){
-        return transacoesFacade.listaTransacoes();
+    public ResponseEntity<List<TransacoesResponseDTO>> listarTransacoes(){
+        return new ResponseEntity<>(transacoesFacade.listaTransacoes(), HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "save successfuly"),
+            @ApiResponse(code = 400, message = "bad request"),
+    })
     @PostMapping
-    public TransacoesResponseDTO cadastrarTransacao(@Valid @RequestBody TransacoesRequestDTO transacoesRequestDTO){
-        return transacoesFacade.cadastrarTransacao(transacoesRequestDTO);
+    public ResponseEntity<TransacoesResponseDTO> cadastrarTransacao(@Valid @RequestBody TransacoesRequestDTO transacoesRequestDTO){
+        return new ResponseEntity<>(transacoesFacade.cadastrarTransacao(transacoesRequestDTO), HttpStatus.CREATED);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "delete successfuly"),
+            @ApiResponse(code = 400, message = "bad request"),
+            @ApiResponse(code = 404, message = "id not found")
+    })
     @DeleteMapping("/{id}")
     public void deletarTransacao(@PathVariable Long id){
         transacoesFacade.deletarTransacao(id);
     }
 
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "find balance successfuly"),
+            @ApiResponse(code = 400, message = "bad request")
+    })
     @GetMapping("/saldo")
-    public SaldoResponseDTO consultarSaldo(TransacoesRequestDTO requestDTO){
+    public ResponseEntity<SaldoResponseDTO> consultarSaldo(TransacoesRequestDTO requestDTO){
         SaldoResponseDTO saldoResponseDTO = transacoesFacade.consultarSaldo(requestDTO.getValorTransacao());
-        return saldoResponseDTO;
+        return new ResponseEntity<>(saldoResponseDTO, HttpStatus.OK);
     }
 
 }
